319c5c1516f841675b33932b59d5ab5b7915f4f7
yasheng-chen <40694531+yasheng-chen@users.noreply.github.com>
2026-09-10 12:42:20 -0500

    nnUNet WMH segmentation

---
+++ Code_segmentation/ppredict_wmh.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gpu_id=$1
+input_folder=$2
+output_folder=$3
+#please point this folder to where you store the model
+model_folder=./Dataset001_wmh_2d3d_flairt1/nnUNetTrainerDA5__nnUNetPlans__3d_fullres
+
+export TORCHDYNAMO_DISABLE=1
+
+CUDA_VISIBLE_DEVICES=$1 nnUNetv2_predict_from_modelfolder -i $input_folder -o $output_folder -m $model_folder -f 0 -c 3d_fullres -chk checkpoint_best.pth
+
